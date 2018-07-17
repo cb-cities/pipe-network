0559e5895c722806a01f4ef3c58dc81401d0c72a
Zhenxiang Su <zs0930@localhost.localdomain>
2018-07-17 10:13:10 -0700

    add node class and test for node class v2

--- include/node.h
+++ include/node.h
@@ -7,89 +7,89 @@
 //! \brief Class that stores the information about nodes
 class Node {
 
-	public:
+ public:
+  // Constructor with id and coordinates
+  //! \param[in] id assign as the id_ of the node
+  //! \param[in] coords coordinates of the node
+  Node(unsigned id, const Eigen::Vector3d& coords) : id_{id} {
+    for (unsigned i = 0; i < coords.size(); i++) coordinates_[i] = coords(i);
+  }
 
-		// Constructor with id and coordinates
-		//! \param[in] id assign as the id_ of the node
-		//! \param[in] coords coordinates of the node
-		Node(unsigned id, const Eigen::Vector3d& coords) : id_{id} {
-			for (unsigned i = 0; i < coords.size(); i++) coordinates_[i] = coords(i);
-		}
+  //! Destructor
+  ~Node() = default;
 
-		//! Destructor
-		~Node() = default;
+  //! Copy constructor
+  Node(const Node&) = delete;
 
-		//! Copy constructor
-		Node(const Node&) = delete;
+  //! Assignment operator
+  Node& operator=(const Node&) = delete;
 
-		//! Assignment operator
-		Node& operator=(const Node&) = delete;
+  //! Move constructor
+  Node(Node&&) = delete;
 
-		//! Move constructor
-		Node(Node&&) = delete;
+  //! Return id
+  //! \retval id_ return id of the node
+  unsigned id() const { return id_; }
 
-		//! Return id
-		//! \retval id_ return id of the node
-		unsigned id() const { return id_; }
+  //! Return coordinates
+  //! \retval coordinates_ return coordinates of the node
+  Eigen::Vector3d coordinates() const { return coordinates_; }
 
-		//! Return coordinates
-		//! \retval coordinates_ return coordinates of the node
-		Eigen::Vector3d coordinates() const { return coordinates_; }
+  //! Return coordinates in a particular direction
+  //! \param[in] dir direction
+  //! \retval coordinates_[dir] return coordinates of the node in a particular
+  //! direction
+  double coord_at_dir(unsigned dir) const { return coordinates_[dir]; }
 
-		//! Return coordinates in a particular direction
-		//! \param[in] dir direction
-		//! \retval coordinates_[dir] return coordinates of the node in a particular direction
-		double coord_at_dir(unsigned dir) const { return coordinates_[dir]; }
+  //! Return number of connection
+  //! \retval nconnections_ return number of connection to the node
+  unsigned nconnections() const { return nconnections_; }
 
-		//! Return number of connection
-		//! \retval nconnections_ return number of connection to the node
-		unsigned nconnections() const { return nconnections_; }
+  //! Assign hydraulic head at the node
+  //! \param[in] head hydraulic head at the node
+  void head(double head) {
+    head_ = head;
+    ishead_ = true;
+  }
 
-		//! Assign hydraulic head at the node
-		//! \param[in] head hydraulic head at the node
-		void head(double head) {
-			head_ = head;
-			ishead_ = true;
-		}
+  //! Return hydraulic head
+  //! \retval head_ return hydraulic head at the node
+  double head() const { return head_; }
 
-		//! Return hydraulic head
-		//! \retval head_ return hydraulic head at the node
-		double head() const { return head_; }
+  //! Return head assignment status
+  //! \retval ishead_ return head assignment status at the node
+  bool ishead() const { return ishead_; }
 
-		//! Return head assignment status
-		//! \retval ishead_ return head assignment status at the node
-		bool ishead() const {return ishead_; }
+  //! Assign discharge at the node
+  //! \param[in] discharge discharge at the node
+  void discharge(double discharge) {
+    discharge_ = discharge;
+    isdischarge_ = true;
+  }
 
-		//! Assign discharge at the node
-		//! \param[in] discharge discharge at the node
-		void discharge(double discharge) {
-			discharge_ = discharge;
-			isdischarge_ = true;
-		}
+  //! Return discharge
+  //! \retval discharge_ return discharge at the node
+  double discharge() const { return discharge_; }
 
-		//! Return discharge
-		//! \retval discharge_ return discharge at the node
-		double discharge() const { return discharge_;}
+  //! Return discharge assignment status
+  //! \retval isdischarge return discharge assignment status at the node
+  bool isdischarge() const { return isdischarge_; }
 
-		//! Return discharge assignment status
-		//! \retval isdischarge return discharge assignment status at the node
-		bool isdischarge() const { return isdischarge_; }
-
-	private:
-		//! node id
-		unsigned id_{std::numeric_limits<unsigned>::max()};
-		//! nodal coordinates
-		Eigen::Vector3d coordinates_;
-		//! number of connection to the node
-		unsigned nconnections_{std::numeric_limits<unsigned>::max()};
-		//! hydraulic head
-		double head_{std::numeric_limits<double>::max()};
-		//! discharge
-		double discharge_{std::numeric_limits<double>::max()};
-		//! whether head is assigned
-		bool ishead_{false};
-		//! whether discharge is assigned
-		bool isdischarge_{false};
+ private:
+  //! node id
+  unsigned id_{std::numeric_limits<unsigned>::max()};
+  //! nodal coordinates
+  Eigen::Vector3d coordinates_;
+  //! number of connection to the node
+  unsigned nconnections_{std::numeric_limits<unsigned>::max()};
+  //! hydraulic head
+  double head_{std::numeric_limits<double>::max()};
+  //! discharge
+  double discharge_{std::numeric_limits<double>::max()};
+  //! whether head is assigned
+  bool ishead_{false};
+  //! whether discharge is assigned
+  bool isdischarge_{false};
 };
 
 #endif  // PIPE_NETWORK_NODE_H_
--- tests/test.cc
+++ tests/test.cc
@@ -3,28 +3,28 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
-TEST_CASE( "Node is checked", "[Node]" ) {
-	Node test (123, {11.2, 22.3, 33.4});
+TEST_CASE("Node is checked", "[Node]") {
+  Node test(123, {11.2, 22.3, 33.4});
 
-	REQUIRE( test.id() == 123 );
-	REQUIRE( test.coordinates()(2) == Approx(33.4).epsilon(1.e-12) );
-	REQUIRE( test.coord_at_dir(1) == Approx(22.3).epsilon(1.e-12) );
-	REQUIRE( test.ishead() == false );
-	REQUIRE( test.isdischarge() == false );
+  REQUIRE(test.id() == 123);
+  REQUIRE(test.coordinates()(2) == Approx(33.4).epsilon(1.e-12));
+  REQUIRE(test.coord_at_dir(1) == Approx(22.3).epsilon(1.e-12));
+  REQUIRE(test.ishead() == false);
+  REQUIRE(test.isdischarge() == false);
 
-	SECTION( "reading and writting hydraulic head" ) {
-	    test.head(110.1);
+  SECTION("reading and writting hydraulic head") {
+    test.head(110.1);
 
-	    REQUIRE( test.head() == Approx(110.1).epsilon(1.e-12) );
-	    REQUIRE( test.ishead() == true );
-	}
+    REQUIRE(test.head() == Approx(110.1).epsilon(1.e-12));
+    REQUIRE(test.ishead() == true);
+  }
 
-	SECTION( "reading and writting discharge" ) {
-	    test.discharge(-23.4);
+  SECTION("reading and writting discharge") {
+    test.discharge(-23.4);
 
-	    REQUIRE( test.discharge() == Approx(-23.4).epsilon(1.e-12) );
-	    REQUIRE( test.isdischarge() == true );
-	}
+    REQUIRE(test.discharge() == Approx(-23.4).epsilon(1.e-12));
+    REQUIRE(test.isdischarge() == true);
+  }
 
-	//test return nconnections
+  // test return nconnections
 }
