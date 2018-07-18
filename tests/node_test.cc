9111dc860154194f0c7c61149306bebf63348dd7
Krishna Kumar <3963513+kks32@users.noreply.github.com>
2018-07-18 07:44:51 +0100

    :pencil: Remove unnecessary ! comments in node_test

--- tests/node_test.cc
+++ tests/node_test.cc
@@ -2,7 +2,7 @@
 
 #include "node.h"
 
-//! \brief Check node class
+// Check node class
 TEST_CASE("Node is checked", "[Node]") {
   // Tolerance
   const double tolerance = 1.e-12;
@@ -15,38 +15,38 @@ TEST_CASE("Node is checked", "[Node]") {
   // Create a node
   auto node = std::make_unique<Node>(id, coords);
 
-  //! Check node id
+  // Check node id
   REQUIRE(node->id() == id);
   
-  //! Check nodal coordinates
+  // Check nodal coordinates
   for (int i = 0; i < coords.size(); ++i)
     REQUIRE(node->coordinates()(i) == Approx(coords(i)).epsilon(tolerance));
 
-  //! Check status of head before it is initialized
+  // Check status of head before it is initialized
   REQUIRE(node->ishead() == false);
 
-  //! Check status of discharge before it is initialized
+  // Check status of discharge before it is initialized
   REQUIRE(node->isdischarge() == false);
 
-  //! Check hydraulic head at the node
+  // Check hydraulic head at the node
   SECTION("check hydraulic head at the node") {
     double head = 110.1;
     node->head(head);
 
-    //! Check hydraulic head at the node
+    // Check hydraulic head at the node
     REQUIRE(node->head() == Approx(head).epsilon(tolerance));
-    //! Check if hydraulic head is assigned at the node
+    // Check if hydraulic head is assigned at the node
     REQUIRE(node->ishead() == true);
   }
 
-  //! Check discharge at the node
+  // Check discharge at the node
   SECTION("Check discharge at the node") {
     double discharge = -23.4;
     node->discharge(discharge);
 
-    //! Check discharge at the node
+    // Check discharge at the node
     REQUIRE(node->discharge() == Approx(discharge).epsilon(tolerance));
-    //! Check if discharge is assigned at the node
+    // Check if discharge is assigned at the node
     REQUIRE(node->isdischarge() == true);
   }
 
